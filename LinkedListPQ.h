15e1595a3979f434bd51261608dd40ca0526a8f4
bren2002 <161561650+bren2002@users.noreply.github.com>
2024-03-25 23:18:39 -0700

    Update LinkedListPQ.h

--- LinkedListPQ.h
+++ LinkedListPQ.h
@@ -4,8 +4,8 @@
  * March 25, 2024
  */
 
-#ifndef LINKEDLISTPQ_H
-#define LINKEDLISTPQ_H
+#ifndef LINKED_LIST_PQ_H
+#define LINKED_LIST_PQ_H
 
 #include "PriorityQueue.h"
 #include "LinkedList.h"
@@ -13,91 +13,41 @@
 template<class T>
 class LinkedListPQ : public PriorityQueue<T> {
 private:
-    LinkedList<T> list;
+    LinkedList<T> pq;
 
 public:
     LinkedListPQ() {}
 
-    ~LinkedListPQ() {}
-
-    bool insert(T object) override {
-        list.append(object);
-        return true;
+    void enqueue(const T &element, int priority) override {
+        pq.addLast(element);
     }
 
-    T remove() override {
-        if (list.isEmpty()) {
-            throw std::invalid_argument("Cannot remove from empty queue");
-        }
-        T highestPriority = list.peekFirst();
-        int highestPriorityIndex = 0;
-        ListIterator<T> it = list.first();
-        int index = 0;
-        while (it.hasNext()) {
-            T current = it.next();
-            if (current < highestPriority) {
-                highestPriority = current;
-                highestPriorityIndex = index;
-            }
-            index++;
-        }
-        return list.removeAt(highestPriorityIndex);
+    T dequeue() override {
+        if (isEmpty())
+            throw std::runtime_error("Error: dequeue from empty priority queue");
+        return pq.removeFirst();
     }
 
-    bool deleteAll(T obj) override {
-        bool deleted = false;
-        ListIterator<T> it = list.first();
-        while (it.hasNext()) {
-            if (it.next() == obj) {
-                it.remove();
-                deleted = true;
-            }
-        }
-        return deleted;
+    const T &peek() const override {
+        if (isEmpty())
+            throw std::runtime_error("Error: peek from empty priority queue");
+        return pq.peekFirst();
     }
 
-    T peek() override {
-        if (list.isEmpty()) {
-            throw std::invalid_argument("Cannot peek from empty queue");
-        }
-        return list.peekFirst();
+    bool isEmpty() const override {
+        return pq.isEmpty();
     }
 
-    bool contains(T obj) override {
-        ListIterator<T> it = list.first();
-        while (it.hasNext()) {
-            if (it.next() == obj) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    int size() override {
-        return list.getSize();
+    int size() const override {
+        return pq.size();
     }
 
     void clear() override {
-        list.makeEmpty();
-    }
-
-    bool isEmpty() override {
-        return list.isEmpty();
-    }
-
-    bool isFull() override {
-        return false; 
-    }
-
-    ListIterator<T> first() override {
-        return list.begin();
-    }
-
-    ListIterator<T> end() override {
-        return list.end();
+        pq.makeEmpty();
     }
 };
 
 #endif
+
 
 
