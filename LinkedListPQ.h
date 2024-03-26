63364f3a25b4b0626c837306a336ff1d3dbeb418
bren2002 <161561650+bren2002@users.noreply.github.com>
2024-03-25 22:53:29 -0700

    Update LinkedListPQ.h

--- LinkedListPQ.h
+++ LinkedListPQ.h
@@ -8,21 +8,22 @@
 #define LINKEDLISTPQ_H
 
 #include "LinkedList.h"
+#include "ListIterator.h"
 
-template <typename T>
+template<typename T>
 class LinkedListPQ {
+private:
     LinkedList<T> list;
 
 public:
-    bool isEmpty() const { return list.isEmpty(); }
-    bool isFull() const { return false; } // Linked List doesn't have a fixed size
-    void insert(const T& data) { list.addLast(data); }
-    T peek() const; // Implement according to your priority queue logic
-    T remove(); // Implement according to your priority queue logic
-    int size() const { return list.size(); }
-    bool contains(const T& data) const { return list.contains(data); }
-    void deleteAll(const T& data) { list.removeAll(data); }
-    void clear() { list.makeEmpty(); }
+    ListIterator<T> first() {
+        return list.first();
+    }
+
+    ListIterator<T> end() {
+        return list.end();
+    }
+
 };
 
 #endif
