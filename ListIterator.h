8032a23371375ec7f30dbe395dfe99125cb854c7
bren2002 <161561650+bren2002@users.noreply.github.com>
2024-03-25 22:12:52 -0700

    Update ListIterator.h

--- ListIterator.h
+++ ListIterator.h
@@ -1,1 +1,35 @@
+/**
+ * ListIterator.h
+ * Brendan Scharmann, 825291656
+ * March 25, 2024
+ */
 
+#ifndef LIST_ITERATOR_H
+#define LIST_ITERATOR_H
+
+#include "Node.h"
+
+template<typename T>
+class ListIterator {
+
+private:
+    Node<T> *current;
+
+public:
+    ListIterator(Node<T> *start) : current(start) {}
+
+    T operator*() {
+        return current->data;
+    }
+
+    ListIterator<T>& operator++() {
+        current = current->next;
+        return *this;
+    }
+
+    bool operator!=(const ListIterator<T>& other) {
+        return current != other.current;
+    }
+};
+
+#endif
