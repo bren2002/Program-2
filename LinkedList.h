0d427ed75d19381c1db210b68c656b881a8ddf10
bren2002 <161561650+bren2002@users.noreply.github.com>
2024-03-25 22:47:12 -0700

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -7,45 +7,74 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
+#include "Node.h"
 #include "ListIterator.h"
 
 template <typename T>
 class LinkedList {
-private:
-    Node<T>* head;
+    Node<T> *head;
+    Node<T> *tail;
 
 public:
-    LinkedList() : head(nullptr) {}
-
-    void insert(const T& data) {
-        Node<T>* newNode = new Node<T>(data);
-        if (!head) {
-            head = newNode;
-        } else {
-            Node<T>* temp = head;
-            while (temp->next) {
-                temp = temp->next;
-            }
-            temp->next = newNode;
-        }
-    }
-
-    ListIterator<T> first() {
-        return ListIterator<T>(head);
-    }
-
-    ListIterator<T> end() {
-        return ListIterator<T>(nullptr);
-    }
-
-    ~LinkedList() {
-        Node<T>* temp;
-        while (head) {
-            temp = head;
-            head = head->next;
-            delete temp;
-        }
-    }
+    LinkedList() : head(nullptr), tail(nullptr) {}
+    bool isEmpty() const { return head == nullptr; }
+    void addFirst(const T& data);
+    void addLast(const T& data);
+    int size() const;
+    bool contains(const T& data) const;
+    int find(const T& data) const;
+    void makeEmpty();
+    void addAt(int index, const T& data);
+    void removeAt(int index);
+    void removeAll(const T& data);
+    ListIterator<T> first() const { return ListIterator<T>(head); }
+    ListIterator<T> end() const { return ListIterator<T>(nullptr); }
+    ~LinkedList();
 };
+
+template <typename T>
+void LinkedList<T>::addFirst(const T& data) {
+    if (isEmpty()) {
+        head = tail = new Node<T>(data);
+    } else {
+        head = new Node<T>(data, head);
+    }
+}
+
+template <typename T>
+void LinkedList<T>::addLast(const T& data) {
+    if (isEmpty()) {
+        head = tail = new Node<T>(data);
+    } else {
+        tail->next = new Node<T>(data);
+        tail = tail->next;
+    }
+}
+
+template <typename T>
+int LinkedList<T>::size() const {
+    int count = 0;
+    Node<T> *current = head;
+    while (current != nullptr) {
+        count++;
+        current = current->next;
+    }
+    return count;
+}
+
+template <typename T>
+LinkedList<T>::~LinkedList() {
+    makeEmpty();
+}
+
+template <typename T>
+void LinkedList<T>::makeEmpty() {
+    while (head != nullptr) {
+        Node<T> *temp = head;
+        head = head->next;
+        delete temp;
+    }
+    tail = nullptr;
+}
 
 #endif
