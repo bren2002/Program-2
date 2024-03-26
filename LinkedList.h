5358043bba47c4af9cd4c22abd2f5036250e7e05
bren2002 <161561650+bren2002@users.noreply.github.com>
2024-03-25 23:09:48 -0700

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -13,13 +13,23 @@
 template<typename T>
 class LinkedList {
 private:
-    Node<T>* head;
-    Node<T>* tail;
+    struct Node {
+        T data;
+        Node* next;
+
+        Node(const T& newData, Node* newNext = nullptr) : data(newData), next(newNext) {}
+    };
+
+    Node* head;
+    Node* tail;
     int size;
 
 public:
     LinkedList() : head(nullptr), tail(nullptr), size(0) {}
-    ~LinkedList();
+
+    ~LinkedList() {
+        makeEmpty();
+    }
 
     bool isEmpty() const {
         return size == 0;
@@ -29,78 +39,144 @@ public:
         return size;
     }
 
-    void append(const T& item) {
+    void addLast(const T& item) {
         if (isEmpty()) {
-            head = new Node<T>(item);
-            tail = head;
+            head = tail = new Node(item);
         } else {
-            tail->next = new Node<T>(item);
+            tail->next = new Node(item);
             tail = tail->next;
         }
         size++;
     }
 
-    void prepend(const T& item) {
-        head = new Node<T>(item, head);
+    void addFirst(const T& item) {
+        head = new Node(item, head);
         if (!tail) {
             tail = head;
         }
         size++;
     }
 
-    void insertAt(int index, const T& item) {
+    void addAt(int index, const T& item) {
         if (index < 0 || index > size) {
             throw std::out_of_range("Index out of range");
         }
         if (index == 0) {
-            prepend(item);
+            addFirst(item);
         } else if (index == size) {
-            append(item);
+            addLast(item);
         } else {
-            Node<T>* current = head;
+            Node* current = head;
             for (int i = 0; i < index - 1; ++i) {
                 current = current->next;
             }
-            current->next = new Node<T>(item, current->next);
+            current->next = new Node(item, current->next);
             size++;
         }
     }
 
-    T removeAt(int index) {
-        if (isEmpty() || index < 0 || index >= size) {
-            throw std::out_of_range("Index out of range");
+    T removeFirst() {
+        if (isEmpty()) {
+            throw std::runtime_error("List is empty");
         }
-        T removedItem;
-        if (index == 0) {
-            removedItem = head->data;
-            Node<T>* temp = head;
-            head = head->next;
-            delete temp;
-            if (!head) {
-                tail = nullptr;
-            }
-        } else {
-            Node<T>* current = head;
-            for (int i = 0; i < index - 1; ++i) {
-                current = current->next;
-            }
-            Node<T>* temp = current->next;
-            removedItem = temp->data;
-            current->next = temp->next;
-            delete temp;
-            if (!current->next) {
-                tail = current;
-            }
+        T removedItem = head->data;
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        if (!head) {
+            tail = nullptr;
         }
         size--;
         return removedItem;
+    }
+
+    T removeLast() {
+        if (isEmpty()) {
+            throw std::runtime_error("List is empty");
+        }
+        T removedItem;
+        if (head == tail) {
+            removedItem = head->data;
+            delete head;
+            head = tail = nullptr;
+        } else {
+            Node* temp = head;
+            while (temp->next != tail) {
+                temp = temp->next;
+            }
+            removedItem = tail->data;
+            delete tail;
+            tail = temp;
+            tail->next = nullptr;
+        }
+        size--;
+        return removedItem;
+    }
+
+    bool contains(const T& item) const {
+        Node* current = head;
+        while (current) {
+            if (current->data == item) {
+                return true;
+            }
+            current = current->next;
+        }
+        return false;
+    }
+
+    int find(const T& item) const {
+        Node* current = head;
+        int index = 0;
+        while (current) {
+            if (current->data == item) {
+                return index;
+            }
+            current = current->next;
+            index++;
+        }
+        return -1; // Not found
+    }
+
+    void removeAll(const T& item) {
+        Node* current = head;
+        Node* prev = nullptr;
+        while (current) {
+            if (current->data == item) {
+                if (prev) {
+                    prev->next = current->next;
+                    delete current;
+                    current = prev->next;
+                } else {
+                    Node* temp = head;
+                    head = head->next;
+                    delete temp;
+                    current = head;
+                }
+                size--;
+            } else {
+                prev = current;
+                current = current->next;
+            }
+        }
+        tail = prev;
+    }
+
+    void makeEmpty() {
+        Node* current = head;
+        while (current) {
+            Node* temp = current;
+            current = current->next;
+            delete temp;
+        }
+        head = tail = nullptr;
+        size = 0;
     }
 
     T getAt(int index) const {
         if (index < 0 || index >= size) {
             throw std::out_of_range("Index out of range");
         }
-        Node<T>* current = head;
+        Node* current = head;
         for (int i = 0; i < index; ++i) {
             current = current->next;
         }
@@ -121,39 +197,7 @@ public:
         return tail->data;
     }
 
-    void removeFirst() {
-        if (isEmpty()) {
-            throw std::runtime_error("List is empty");
-        }
-        Node<T>* temp = head;
-        head = head->next;
-        delete temp;
-        if (!head) {
-            tail = nullptr;
-        }
-        size--;
-    }
-
-    void removeLast() {
-        if (isEmpty()) {
-            throw std::runtime_error("List is empty");
-        }
-        if (head == tail) {
-            delete head;
-            head = tail = nullptr;
-        } else {
-            Node<T>* temp = head;
-            while (temp->next != tail) {
-                temp = temp->next;
-            }
-            delete tail;
-            tail = temp;
-            tail->next = nullptr;
-        }
-        size--;
-    }
-
-    ListIterator<T> begin() const {
+    ListIterator<T> first() const {
         return ListIterator<T>(head);
     }
 
@@ -163,4 +207,6 @@ public:
 };
 
 #endif
+
+
 
