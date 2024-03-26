40a1b2c2b79b950a70bbddcb5b4114b717ab68e2
bren2002 <161561650+bren2002@users.noreply.github.com>
2024-03-25 22:11:52 -0700

    Create Node.h

---
+++ Node.h
@@ -0,0 +1,20 @@
+/**
+ * Node.h
+ * Brendan Scharmann, 825291656
+ * March 25, 2024
+ */
+
+
+#ifndef NODE_H
+#define NODE_H
+
+template<typename T>
+class Node {
+public:
+    T data;
+    Node<T> *next;
+    
+    Node(T newData) : data(newData), next(nullptr) {}
+};
+
+#endif
